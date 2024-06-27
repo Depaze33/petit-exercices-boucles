86adf89c5bc9802d10c47a0fc1a3040be510c7cf
Clarisse Otmani <clarisseotm@gmail.com>
2024-06-27 09:04:05 +0200

    first commit Ex1

---
+++ compteurDeUnADix.java
@@ -0,0 +1,15 @@
+public class compteurDeUnADix {
+
+    public static void main(String args[]) {
+        UnADix();
+    }
+
+    public static void UnADix() {
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("le chiffre est" + i);
+
+        }
+
+    }
+}
